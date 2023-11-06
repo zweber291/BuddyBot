f9e0c67145a4cf92fc6ad9defaa95e6dbc06b947
zweber291 <zweber291@gmail.com>
2023-11-06 02:21:02 -0500

    Add new start.js for starting the bot.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
-const { token, ntfy_url } = require('./config.json');
+const { token } = require('./config.json');
 const { Player } = require('discord-player');
 
 const client = new Client({ intents: [
---
+++ start.js
@@ -0,0 +1,24 @@
+const child_process = require('child_process');
+const child = child_process.spawn('node index.js', [], { shell: true});
+const { ntfy_url } = require('./config.json');
+const fetch = require('node-fetch');
+
+child.on('exit', async (code) => {
+    fetch(ntfy_url, {
+        method: 'POST',
+        body: `Buddybot offline!`,
+        headers: {
+            'Title': `Code: ${code}`,
+            'Priority': 'urgent'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Child process exited with code', code);
+        process.exit(1);
+    })
+    .catch(error => {
+        console.error('Error sending notification:', error);
+        process.exit(1);
+    });
+  });
